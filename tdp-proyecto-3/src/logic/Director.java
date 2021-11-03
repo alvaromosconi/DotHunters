8f80f6902a1766991be75d65ccabcb8889854e15
Melisa Moya <meelmoya@gmail.com>
2021-11-03 20:55:27 -0300

    Finalización de carga de paredes de mapa1

--- tdp-proyecto-3/src/logic/Director.java
+++ tdp-proyecto-3/src/logic/Director.java
@@ -19,7 +19,7 @@ public void constructLevelOne(Builder b) {
 	List<Entity> walls = new ArrayList<Entity>();
 	
 	
-	Entity player = new MainCharacter(400, 400, "/assets/mario1.gif");
+	Entity player = new MainCharacter(400, 396, "/assets/mario1.gif");
 	
 	
 	
@@ -48,9 +48,52 @@ public void constructLevelOne(Builder b) {
 	Entity sw12 = new Wall(264, 660, 44, 44);
 	
 	
-	Entity ne1 = new Wall(550, 0, 596, 44);
+	Entity n1 = new Wall(396, 88, 396, 44);
+	Entity n2 = new Wall(308, 176, 572, 44);
+	Entity n3 = new Wall(440, 264, 132, 44);
+	Entity n4 = new Wall(440, 308, 44, 88);
+	Entity n5 = new Wall(484, 352, 264, 44);
+	Entity n6 = new Wall(704, 308, 44, 88); 
+	Entity n7 = new Wall(616, 264, 132, 44);
 	
-	walls.addAll(Arrays.asList(nw1, nw2, nw3, nw4, nw5, nw6, nw7, nw8, nw9, nw10, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, sw11, sw12, ne1));	
+	Entity s1 = new Wall(440, 440, 308, 44);
+	Entity s2 = new Wall(440, 484, 44, 88);
+	Entity s3 = new Wall(484, 528, 44, 44);
+	Entity s4 = new Wall(572, 528, 44, 220);
+	
+	Entity ne1 = new Wall(836, 88, 264, 44);
+	Entity ne2 = new Wall(924, 132, 44, 176);
+	Entity ne3 = new Wall(836, 264, 132, 44);
+	Entity ne4 = new Wall(792, 264, 44, 132);
+	Entity ne5 = new Wall(836, 352, 176, 44);
+	Entity ne6 = new Wall(1056, 352, 132, 44);
+	Entity ne7 = new Wall(1012, 176, 88, 132);
+	Entity ne8 = new Wall(1144, 0, 44, 352);
+	Entity ne9 = new Wall(572, 0, 572, 44);
+	
+	Entity se1 = new Wall(704, 484, 44, 88);
+	Entity se2 = new Wall(660, 528, 44, 44);
+	Entity se3 = new Wall(660, 616, 176, 44);
+	Entity se4 = new Wall(792, 572, 132, 44);
+	Entity se5 = new Wall(792, 440, 176, 88);
+	Entity se6 = new Wall(968, 440, 44, 44);
+	Entity se7 = new Wall(1056, 440, 132, 44);
+	Entity se8 = new Wall(1144, 484, 44, 352);
+	Entity se9 = new Wall(1012, 528, 88, 44);
+	Entity se10 = new Wall(968, 572, 132, 88);
+	Entity se11 = new Wall(1012, 704, 132, 44);
+	Entity se12 = new Wall(660, 704, 308, 44);
+	Entity se13 = new Wall(880, 660, 44, 44);
+	Entity se14 = new Wall(572, 792, 572, 44);
+	
+	
+	
+	walls.addAll(Arrays.asList(nw1, nw2, nw3, nw4, nw5, nw6, nw7, nw8, nw9, nw10, 
+							   sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, sw11, sw12, 
+							   n1, n2, n3, n4, n5, n6, n7,
+							   s1, s2, s3, s4,
+							   ne1, ne2, ne3, ne4, ne5, ne6, ne7, ne8, ne9,
+							   se1, se2, se3, se4, se5, se6, se7, se8, se9, se10, se11, se12, se13,se14));
 
 	for (int i = 0; i < components.size(); i++) {
 		
